02038482e8d0aa18d65f2d52c0991f7995383c72
Dinar Agzyamov <mail@deenar.ru>
2022-02-01 21:35:25 +0300

    add markup, new lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleryMarkup(galleryItems) {
+  const markup = galleryItems
+    .map((galleryItem) => {
+      return `
+        <a class="gallery__item" href="${galleryItem.original}">
+        <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+        </a>
+        `;
+    })
+    .join("");
+  gallery.insertAdjacentHTML("afterbegin", markup);
+}
+createGalleryMarkup(galleryItems);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
