5c7be54f419d1bb6381430108aa21ca320be7c90
Dinar Agzyamov <mail@deenar.ru>
2022-02-01 20:51:42 +0300

    add large image close function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,12 +28,22 @@ gallery.addEventListener("click", openLargeImg);
 
 function openLargeImg(event) {
   event.preventDefault();
+
   if (event.target.nodeName !== "IMG") {
     return;
   }
+
   const largeImgUrl = event.target.dataset.source;
   const instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}" width="800" height="600">`);
   instance.show();
+
+  document.addEventListener("keydown", closeLargeImg);
+  function closeLargeImg(event) {
+    if (event.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  }
 }
 console.log(galleryItems);
