c998c23602491db0e9f28ea38fd7815fac509553
Dinar Agzyamov <mail@deenar.ru>
2022-02-01 20:38:45 +0300

    add markup create function, basic-lightbox css & js files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,11 @@
 import { galleryItems } from "./gallery-items.js";
+
 const gallery = document.querySelector(".gallery");
-const galleryImage = galleryItems
-  .map(
-    (galleryItem) =>
-      `
+
+function createGalleryMarkup(galleryItems) {
+  const markup = galleryItems
+    .map((galleryItem) => {
+      return `
       <div class="gallery__item">
       <a class="gallery__link" href="${galleryItem.original}">
       <img
@@ -14,8 +16,24 @@ const galleryImage = galleryItems
       />
       </a>
       </div>
-      `
-  )
-  .join("");
-gallery.insertAdjacentHTML("afterbegin", galleryImage);
+      `;
+    })
+    .join("");
+  gallery.insertAdjacentHTML("afterbegin", markup);
+}
+
+createGalleryMarkup(galleryItems);
+
+gallery.addEventListener("click", openLargeImg);
+
+function openLargeImg(event) {
+  e.prevenDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const largeImgUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`);
+  instance.show();
+}
 console.log(galleryItems);
